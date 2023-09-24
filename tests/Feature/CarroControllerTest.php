f405dae1727d43036cc7f9c87da7a2243c5e70f3
Matico <195817@upf.br>
2023-09-24 20:15:44 -0300

    projeto completo final

--- tests/Feature/CarroControllerTest.php
+++ tests/Feature/CarroControllerTest.php
@@ -35,25 +35,61 @@ class CarroControllerTest extends TestCase
     $this->assertDatabaseHas('carros', ['modelo' => 'Carro Teste']);
 }
 
-    public function test_carro_edit()
+public function test_carro_edit()
 {
-    
+    // Crie um usuário para autenticar
     $user = User::factory()->create();
-
-   
     $this->actingAs($user);
 
-  
+    // Crie um carro para editar
     $carro = Carro::factory()->create();
 
- 
+    // Acesse a página de edição do carro
     $response = $this->get("/carros/editar/{$carro->id}");
-
     $response->assertStatus(200);
-
-   
 }
 
+public function test_carro_update()
+{
+    // Crie um usuário para autenticar
+    $user = User::factory()->create();
+    $this->actingAs($user);
 
-    
+    // Crie um carro para atualizar
+    $carro = Carro::factory()->create();
+
+    // Envie uma solicitação de atualização para o controlador
+    $response = $this->put("/carros/editar/{$carro->id}", [
+        'modelo' => 'Novo Modelo',
+        'placa' => 'XYZ-5678',
+        'marca_id' => $carro->marca_id, // Mantenha a mesma marca
+        'ano' => 2023,
+    ]);
+
+    // Verifique se a atualização foi bem-sucedida
+    $response->assertStatus(302);
+    $this->assertDatabaseHas('carros', [
+        'id' => $carro->id,
+        'modelo' => 'Novo Modelo',
+        'placa' => 'XYZ-5678',
+        'ano' => 2023,
+    ]);
+}
+
+public function test_carro_delete()
+{
+    // Crie um usuário para autenticar
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    // Crie um carro para excluir
+    $carro = Carro::factory()->create();
+
+    // Envie uma solicitação de exclusão para o controlador
+    $response = $this->get("/carros/delete/{$carro->id}");
+
+    // Verifique se o carro foi excluído com sucesso
+    $response->assertStatus(302);
+    $this->assertDatabaseMissing('carros', ['id' => $carro->id]);
+}
 }
--- tests/Feature/MarcaControllerTest.php
+++ tests/Feature/MarcaControllerTest.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\Marca;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -36,18 +34,42 @@ class MarcaControllerTest extends TestCase
 }
 
 
-    public function test_marca_edit()
+public function test_marca_update()
 {
-
+    // Crie um usuário para autenticar
     $user = User::factory()->create();
+    $this->actingAs($user);
 
+    // Crie uma marca para atualizar
     $marca = Marca::factory()->create();
 
-    $response = $this->actingAs($user)->get("/marcas/editar/{$marca->id}");
-    $response->assertStatus(200);
+    // Envie uma solicitação de atualização para o controlador
+    $response = $this->put("/marcas/editar/{$marca->id}", [
+        'nome' => 'Nova Marca',
+    ]);
 
-  
+    // Verifique se a atualização foi bem-sucedida
+    $response->assertStatus(302);
+    $this->assertDatabaseHas('marcas', [
+        'id' => $marca->id,
+        'nome' => 'Nova Marca',
+    ]);
 }
 
+public function test_marca_delete()
+{
+    // Crie um usuário para autenticar
+    $user = User::factory()->create();
+    $this->actingAs($user);
 
+    // Crie uma marca para excluir
+    $marca = Marca::factory()->create();
+
+    // Envie uma solicitação de exclusão para o controlador
+    $response = $this->get("/marcas/delete/{$marca->id}");
+
+    // Verifique se a marca foi excluída com sucesso
+    $response->assertStatus(302);
+    $this->assertDatabaseMissing('marcas', ['id' => $marca->id]);
+}
 }
